22b64d85c31e167b73064a35ade7ba91482dd445
yingwu <dongfangnan@unisound.com>
2019-07-19 12:13:13 +0800

    d

--- src/main/java/com/self/transac/distribut_client/DistributClientApplication.java
+++ src/main/java/com/self/transac/distribut_client/DistributClientApplication.java
@@ -1,9 +1,15 @@
 package com.self.transac.distribut_client;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class} )
+@MapperScan(basePackages = {"com.self.transac.distribut_client.dao"})
+@EnableAutoConfiguration
 public class DistributClientApplication {
 
 	public static void main(String[] args) {
